c00eca5c4ae096918e5e5150e226889b2f079852
FrancescoMinichiello <fraps2278@gmail.com>
2024-09-11 14:58:55 +0200

    printed one email with axios

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            email: [],
+            apiUrl: "https://flynn.boolean.careers/exercises/api/random/mail"
+        }
+    },
+    methods: {
+        getRandomEmail() {
+            axios.get(this.apiUrl)
+                .then((response) => {
+                    this.email = (response.data.response)
+                }
+                );
+        }
+    },
+    mounted() {
+        this.getRandomEmail();
+    },
+}).mount('#app')
